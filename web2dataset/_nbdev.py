ff5d073db1a672fc184e4254ab78f1dff2fc8d9f
sami jaghouar <sami.jaghouar@hotmail.fr>
2022-03-05 17:19:04 +0100

    feat: refactor ImageDownloader

--- web2dataset/_nbdev.py
+++ web2dataset/_nbdev.py
@@ -4,6 +4,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Downloader": "Downloader.ipynb",
          "DownloaderError": "Downloader.ipynb",
+         "ImageDownloader": "Downloader.ipynb",
          "GoogleImageDownloaderError": "Downloader.ipynb",
          "GoogleImageDownloader": "Downloader.ipynb",
          "ImageCleaner": "cleaner.ipynb"}
